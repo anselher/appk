719c44c4f446a0132cda4a7fe75d35cba48fac51
anselher <anselher@gmail.com>
2022-07-25 09:53:37 -0400

    Update and rename exctvplay2.php to cc3.php

---
+++ cc3.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DW en Español</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv1.22.css" rel="stylesheet">
+<link href="exctvplay2.css" rel="stylesheet">
+<script src="https://player2.h-cdn.com/hola_player.js?customer=demo"></script>
+
+</head>
+<body>
+
+<div id="Html1" style="position:absolute;left:0px;top:0px;width:630px;height:360px;z-index:1">
+<div id="maincontainer" class="container-fluid mrt20 mrb20 clearfix maincontainer"> <!-- start header div 1, will end in footer -->
+	<div id="content" class="container"> <!-- start header div 2, will end in footer -->
+		<div class="row"> <!-- start header div 3, will end in footer -->
+		
+
+<div class="col-md-12">
+	<div class="left-content sngl-pg" >
+	<div id="post-165" class="clearfix single-posst post-165 page type-page status-publish hentry" itemscope itemtype="http://schema.org/CreativeWork">
+	<div class="content-first">
+				
+					<div class="content-second">
+				<h1 class="the-title entry-title" itemprop="headline"></h1>
+			</div>
+						
+		<div class="content-third" itemprop="text">
+					
+			<div class="entry-content">
+
+				
+				
+			<script type='text/javascript' src='https://maindigitalstream.com/jwplayer/jwplayer.js?2.2.3.5'></script>
+<script>jwplayer.key="8gDDWtjPpyFgSWsZPjDk4MV6CVI0wEVf2xp7vS7FZUw=";</script><div id='MediaPlayerOverview'>Loading&#8230;</div>
+<script type='text/javascript'>
+  jwplayer('MediaPlayerOverview').setup({
+ sources: [
+     {file: "https://vcp.myplaytv.com/explosioncreativa/explosioncreativa/playlist.m3u8?PlaylistM3UCL"},
+   {file: "https://vcp.myplaytv.com/explosioncreativa/explosioncreativa/playlist.m3u8"}
+    ],
+ rtmp: {
+  bufferlength: 3,
+ },
+ 'autostart': 'true',
+ 'width': '100%',
+ 'aspectratio': '16:9',
+ 'height': '100%',
+ 'fallback': false,
+ 'androidhls': true,
+ 'primary': 'html5',
+ logo: {
+  file: 'https://img1.wsimg.com/isteam/ip/5dbf1322-b57c-4d22-ad63-d4e4f3c4303d/LOGO%20EXC%201.png/:/rs=h:30,cg:true,m/cr=h:30/qt=q:95', //watermark image path
+  link: 'https://excve.com/', //link url on watermark image
+  position: 'top-left' //position of watermark on player
+  },
+ });
+</script>
+<!--CusAds0-->
+			</div>						
+		</div>					
+	</div>
+</div>
+	</div>
+</div>	
+		</div> <!-- end header div 3 -->
+	</div> <!-- end header div 2 -->
+</div> <!-- end header div 1 --></div>
+</body>
+</html>
