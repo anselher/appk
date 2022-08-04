3831adf82b64e5e1c713270b407c552135d13a83
anselher <anselher@gmail.com>
2022-08-04 05:39:12 -0400

    Add files via upload

--- page9.php
+++ page9.php
@@ -2,55 +2,61 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Telesur</title>
+<title>page19</title>
 <meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="plyr.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
-<link href="365tv1.22.css" rel="stylesheet">
+<link href="365tv2.22.css" rel="stylesheet">
 <link href="page9.css" rel="stylesheet">
 <script src="jquery-3.6.0.min.js"></script>
-<script src="plyr.min.js"></script>
-<script src="jquery-ui.min.js"></script>
-<script src="https://www.youtube.com/iframe_api"></script>
-<script>
-var playerYouTube1;
-function onYouTubeIframeAPIReady() {
-   playerYouTube1 = new YT.Player('YouTube1', {
-      events: {
-         'onStateChange': onStateChangeYouTube1
-      }
-   });
-}
-function onStateChangeYouTube1(event) {
-   switch(event.data) {
-      case 3:
-         Toggle('wb_YouTube1', 'pulsate', 500);
-         break;
-   }
-}
-</script>
-<script src="wwb17.min.js"></script>
 <script>
 $(document).ready(function()
 {
-   const plyrYouTube1 = new Plyr('#wb_YouTube1', {loadSprite: false, iconUrl: 'plyr.svg'});
+   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {$('#preloader').remove();}
+});
+$(window).on('load', function()
+{
+   $('#preloader').remove();
 });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/level-selector@latest/dist/level-selector.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/clappr-chromecast-plugin@latest/dist/clappr-chromecast-plugin.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/clappr-pip@latest/dist/clappr-pip.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/dash-shaka-playback@latest/dist/dash-shaka-playback.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/clappr-playback-rate-plugin@latest/dist/clappr-playback-rate-plugin.min.js"></script>
+
 </head>
 <body>
-<div id="FlexGrid1">
-<div class="header">
-<div id="wb_YouTube1" style="display:inline-block;width:100%;z-index:0;">
-<iframe id="YouTube1" src="https://www.youtube.com/embed/fdsG8BmMd2k?rel=0&amp;autoplay=1&amp;modestbranding=1&amp;enablejsapi=1&amp;autohide=0"></iframe>
-
-</div>
-</div>
-<div class="main">
-</div>
-<div class="footer">
-</div>
-</div>
 <a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:630px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="Html1" style="position:absolute;left:0px;top:0px;width:630px;height:355px;z-index:1">
+<div id="player"></div>
+    <script>
+      var player = new Clappr.Player(
+        {
+          source: 'https://cetl.cdn.rcs.net.ar/mnp/h2/output.mpd',
+          parentId: '#player',
+          plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback, Clappr.MediaControl],
+          events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},},
+          height: 355,
+          width: 630,
+          autoPlay: 'true',
+          shakaConfiguration: {
+          manifest: {retryParameters: {maxAttempts: Infinity}},
+          streaming: {retryParameters: {maxAttempts: Infinity}},
+          drm: {retryParameters: {maxAttempts: Infinity}},
+          preferredAudioLanguage: 'es-VE',
+          },
+          shakaOnBeforeLoad: function(shaka_player) {
+            // shaka_player.getNetworkingEngine().registerRequestFilter() ...
+          },
+          parentId: '#player'
+        });
+    </script></div>
+<div id="preloader"></div>
 </body>
 </html>
