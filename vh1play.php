5381ae83218c59b513141a3935d4df84b06ae84e
anselher <anselher@gmail.com>
2022-08-04 04:08:48 -0400

    Add files via upload

---
+++ vh1play.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>page19</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv2.22.css" rel="stylesheet">
+<link href="vh1play.css" rel="stylesheet">
+<script src="jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {$('#preloader').remove();}
+});
+$(window).on('load', function()
+{
+   $('#preloader').remove();
+});
+</script>
+<script src="//cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js"></script>
+
+<script src="//cdn.jsdelivr.net/npm/level-selector@latest/dist/level-selector.min.js"></script>
+
+<script src="//cdn.jsdelivr.net/npm/clappr-chromecast-plugin@latest/dist/clappr-chromecast-plugin.min.js"></script>
+
+<script src="//cdn.jsdelivr.net/npm/clappr-pip@latest/dist/clappr-pip.min.js"></script>
+
+<script src="//cdn.jsdelivr.net/npm/dash-shaka-playback@latest/dist/dash-shaka-playback.min.js"></script>
+
+<script src="//cdn.jsdelivr.net/npm/clappr-playback-rate-plugin@latest/dist/clappr-playback-rate-plugin.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/clappr/clappr@latest/dist/clappr.min.js"></script>
+<script charset="utf-8" src="./dist/dash-shaka-playback.js"> </script>
+<script src="//cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
+
+</head>
+<body>
+<div id="player"><div data-player="" tabindex="9999" class="" style="height: 100%; width: 100%;"><div class="container" data-container=""><div data-spinner="" class="spinner-three-bounce" style="display: none;"><div data-bounce1=""></div><div data-bounce2=""></div><div data-bounce3=""></div>
+</div><div class="player-poster" data-poster="" style="display: none;"><div class="play-wrapper" data-poster=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-poster="" class="poster-icon" style="display: none;"><path fill="#010101" d="M1.425.35L14.575 8l-13.15 7.65V.35z"></path></svg></div>
+</div><video data-html5-video="" preload="metadata" src="blob:https://todovieneok.cf/3d479614-0f63-47c5-846c-476db71e6972"></video></div><div class="media-control live media-control-hide" data-media-control="" style=""><div class="media-control-background" data-background=""></div>
+<div class="media-control-layer" data-controls="">
+  
+  
+  
+  
+  
+  
+  
+  <div class="media-control-center-panel" data-media-control="">
+    
+      <div class="bar-container seek-disabled" data-seekbar="">
+        <div class="bar-background" data-seekbar="">
+          <div class="bar-fill-1" data-seekbar="" style="left: 0%;"></div>
+          <div class="bar-fill-2" data-seekbar="" style="width: 100%;"></div>
+          <div class="bar-hover" data-seekbar=""></div>
+        </div>
+        <div class="bar-scrubber" data-seekbar="" style="left: 100%;">
+          <div class="bar-scrubber-icon" data-seekbar=""></div>
+        </div>
+      </div>
+  
+  </div>
+  
+  
+  <div class="media-control-left-panel" data-media-control="">
+    
+    <button type="button" class="media-control-button media-control-icon stopped" data-playstop="" aria-label="playstop"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill-rule="evenodd" clip-rule="evenodd" fill="#010101" d="M1.712 1.24h12.6v13.52h-12.6z"></path></svg></button>
+  
+  <div class="dvr-controls" data-dvr-controls=""><div class="live-info">vivo</div>
+<button type="button" class="live-button" aria-label="volver en vivo">volver en vivo</button>
+</div></div>
+  
+  
+  <div class="media-control-right-panel" data-media-control="">
+    
+    <button type="button" class="media-control-button media-control-icon" data-fullscreen="" aria-label="fullscreen"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="#010101" d="M7.156 8L4 11.156V8.5H3V13h4.5v-1H4.844L8 8.844 7.156 8zM8.5 3v1h2.657L8 7.157 8.846 8 12 4.844V7.5h1V3H8.5z"></path></svg></button><button class="media-control-button media-control-icon pip-button" style="float: right; height: 100%;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" style="fill: none;"></path><path d="M19 11h-8v6h8v-6zm4 10V3H1v18h22zm-2-1.98H3V4.97h18v14.05z"></path></svg></button><div class="cc-controls" data-cc-controls=""><button type="button" class="cc-button media-control-button media-control-icon" data-cc-button="" aria-label="cc-button"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 49 41.8" style="enable-background:new 0 0 49 41.8;" xml:space="preserve"><path d="M47.1,0H3.2C1.6,0,0,1.2,0,2.8v31.5C0,35.9,1.6,37,3.2,37h11.9l3.2,1.9l4.7,2.7c0.9,0.5,2-0.1,2-1.1V37h22.1 c1.6,0,1.9-1.1,1.9-2.7V2.8C49,1.2,48.7,0,47.1,0z M7.2,18.6c0-4.8,3.5-9.3,9.9-9.3c4.8,0,7.1,2.7,7.1,2.7l-2.5,4 c0,0-1.7-1.7-4.2-1.7c-2.8,0-4.3,2.1-4.3,4.3c0,2.1,1.5,4.4,4.5,4.4c2.5,0,4.9-2.1,4.9-2.1l2.2,4.2c0,0-2.7,2.9-7.6,2.9 C10.8,27.9,7.2,23.5,7.2,18.6z M36.9,27.9c-6.4,0-9.9-4.4-9.9-9.3c0-4.8,3.5-9.3,9.9-9.3C41.7,9.3,44,12,44,12l-2.5,4 c0,0-1.7-1.7-4.2-1.7c-2.8,0-4.3,2.1-4.3,4.3c0,2.1,1.5,4.4,4.5,4.4c2.5,0,4.9-2.1,4.9-2.1l2.2,4.2C44.5,25,41.9,27.9,36.9,27.9z"></path></svg></button>
+<ul style="display: none;">
+  
+  <li><a href="#" data-cc-select="-1">Discapacitado</a></li>
+  
+</ul>
+</div>
+  
+      <div class="drawer-container" data-volume="">
+        <div class="drawer-icon-container" data-volume="">
+          <div class="drawer-icon media-control-icon" data-volume=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill-rule="evenodd" clip-rule="evenodd" fill="#010101" d="M11.5 11h-.002v1.502L7.798 10H4.5V6h3.297l3.7-2.502V4.5h.003V11zM11 4.49L7.953 6.5H5v3h2.953L11 11.51V4.49z"></path></svg></div>
+          <span class="drawer-text" data-volume=""></span>
+        </div>
+        
+    <div class="bar-container volume-bar-hide" data-volume="">
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+      <div class="segmented-bar-element fill" data-volume=""></div>
+    
+    </div>
+  
+      </div>
+  
+    <button type="button" class="media-control-button media-control-icon" data-hd-indicator="" aria-label="hd-indicator"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="#010101" d="M5.375 7.062H2.637V4.26H.502v7.488h2.135V8.9h2.738v2.848h2.133V4.26H5.375v2.802zm5.97-2.81h-2.84v7.496h2.798c2.65 0 4.195-1.607 4.195-3.77v-.022c0-2.162-1.523-3.704-4.154-3.704zm2.06 3.758c0 1.21-.81 1.896-2.03 1.896h-.83V6.093h.83c1.22 0 2.03.696 2.03 1.896v.02z"></path></svg></button>
+  
+  <div class="level_selector" data-level-selector=""><button data-level-selector-button="" class="">636.607Kbps</button>
+<ul style="display: none;">
+  
+  <li class=""><a href="#" data-level-selector-select="-1">AUTO</a></li>
+  
+    <li class="current"><a href="#" data-level-selector-select="0">636.607Kbps</a></li>
+  
+    <li class=""><a href="#" data-level-selector-select="1">1775.892Kbps</a></li>
+  
+    <li class=""><a href="#" data-level-selector-select="2">2736.607Kbps</a></li>
+  
+    <li class=""><a href="#" data-level-selector-select="3">6300Kbps</a></li>
+  
+</ul>
+</div><button class="chromecast-button" type="button" style="display: none;"><svg xmlns="http://www.w3.org/2000/svg"><g id="Page-1" fill="none" fill-rule="evenodd"><g id="ic_cast_black_24dp"><g id="ic_remove_circle_white_24dp"><path d="M1 18v3h3c0-1.66-1.34-3-3-3zm0-4v2c2.76 0 5 2.24 5 5h2c0-3.87-3.13-7-7-7zm0-4v2c4.97 0 9 4.03 9 9h2c0-6.08-4.93-11-11-11zm20-7H3c-1.1 0-2 .9-2 2v3h2V5h18v14h-7v2h7c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" id="cast" fill="#000"></path><path id="bounds" d="M0 0h24v24H0z"></path></g></g></g></svg></button></div>
+  
+</div>
+<div class="seek-time" data-seek-time="" style="display: none; left: -100%;"><span data-seek-time=""></span>
+<span data-duration="" style="display: none;"></span>
+</div></div></div></div>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:630px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="Html1" style="position:absolute;left:0px;top:0px;width:630px;height:355px;z-index:1">
+    <div id="player"></div>
+    <script charset="utf-8">
+        window.onload = function () {
+            var onBeforeLoad = function (shaka_player) {
+                shaka_player.getNetworkingEngine().registerRequestFilter(function (type, request) {
+                    if (type == 2) { // shaka.net.NetworkingEngine.RequestType.LICENSE
+                        // do something
+                    }
+                });
+                shaka_player.getNetworkingEngine().registerResponseFilter(function (type, response) {
+                    if (type == 2) { // shaka.net.NetworkingEngine.RequestType.LICENSE
+                        // do something
+                    }
+                });
+            }
+
+            var player = new Clappr.Player({
+                parentId: '#player', autoPlay: true,
+                plugins: [DashShakaPlayback, LevelSelector],
+                shakaConfiguration: {
+                    preferredAudioLanguage: 'pt-BR',
+                    streaming: {
+                        rebufferingGoal: 15
+                    }
+                },
+                shakaOnBeforeLoad: onBeforeLoad,
+                //source: 'https://cetl.cdn.rcs.net.ar/mnp/universal/output.mpd?format=mp4&session_id=25020', //LIVE
+                source: 'https://cetl.cdn.rcs.net.ar/mnp/universal/output.mpd', //multi audio, sub
+                //source: 'https://cetl.cdn.rcs.net.ar/mnp/universal/output.mpd', //VOD multiple qualities
+                //source: 'https://cetl.cdn.rcs.net.ar/mnp/universal/output.mpd', //SUB
+                //source: 'https://cetl.cdn.rcs.net.ar/mnp/universal/output.mpd', //VOD multiple audios
+                //source: 'https://cetl.cdn.rcs.net.ar/mnp/universal/output.mpd', //VOD subtitle
+                //source: 'https://cetl.cdn.rcs.net.ar/mnp/universal/output.mpd', //4K
+                // for more examples go to: http://dashif.org/reference/players/javascript/v1.5.1/samples/dash-if-reference-player/index.html and https://shaka-player-demo.appspot.com/
+                height: 355,
+                width: 630,
+            })
+        }
+    </script>
+</div>
+<div id="preloader"></div>
+</body>
+</html>
