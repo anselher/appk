d907fd76adcee264d9792a5204f33622b157a2c2
anselher <anselher@gmail.com>
2022-08-04 19:02:26 -0400

    Add files via upload

---
+++ demos/front.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Bienvenid@s</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 - https://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv2.22.css" rel="stylesheet">
+<link href="front.css" rel="stylesheet">
+<link href="estilos.css" rel="stylesheet">
+<script>
+ 
+function launchFullScreen(element) {
+  if(element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
+// Lanza en pantalla completa en navegadores que lo soporten
+ function cancelFullScreen() {
+     if(document.cancelFullScreen) {
+         document.cancelFullScreen();
+     } else if(document.mozCancelFullScreen) {
+         document.mozCancelFullScreen();
+     } else if(document.webkitCancelFullScreen) {
+         document.webkitCancelFullScreen();
+     }
+}
+</script>
+
+</head>
+<body>
+<div id="space"><br></div>
+<div id="container">
+<div id="wb_EmbeddedPage1" style="position:absolute;left:0px;top:0px;width:872px;height:1160px;z-index:6;">
+<div id="FlexGrid1">
+<div class="cabeza">
+</div>
+<div class="header">
+</div>
+<div class="espacio1">
+</div>
+<div class="espaciocentro">
+</div>
+<div class="espaciosidebar">
+</div>
+<div class="navmenu">
+</div>
+<div class="infocanal">
+</div>
+<div class="espacio2">
+</div>
+<div class="screentv">
+<iframe name="InlineFrameScreen" id="InlineFrameScreen" src="./images/imagenes.html" scrolling="no"></iframe>
+</div>
+<div class="separador5">
+</div>
+<div class="section1">
+<iframe name="InlineFrameSidebar2" id="InlineFrameSidebar2" class="h6" src="./menu_plan_basico.php" scrolling="no"></iframe>
+</div>
+<div class="section2">
+</div>
+<div class="screencontrol">
+<iframe name="InlineFrameCon" id="InlineFrameCon" src="./mathematics.php" scrolling="no"></iframe>
+</div>
+<div class="section4">
+</div>
+<div class="roller">
+<iframe name="InlineFrameBannerAds" id="InlineFrameBannerAds" src="./banner.php" scrolling="no"></iframe>
+</div>
+<div class="espaciofoot">
+</div>
+<div class="espaciofoot2">
+</div>
+<div class="section3">
+</div>
+<div class="section6">
+</div>
+<div class="section5">
+</div>
+<div class="blogger">
+</div>
+<div class="section7">
+</div>
+<div class="footer">
+</div>
+</div>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:0px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+</div>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:0px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="wb_Breadcrumb1" style="position:absolute;left:0px;top:111px;width:629px;height:20px;z-index:8;vertical-align:top;">
+<ul id="Breadcrumb1">
+<li><a href="./../index.php" title="Inicio"><i class="fa fa-plus-square-o">&nbsp;</i></a></li>
+<li><a href="./../noticias.php">NOTICIAS</a></li>
+<li><a href="./../variedades.php">VARIEDADES</a></li>
+<li><a href="./../documentales.php">DOCUMENTALES</a></li>
+<li><a href="./../series.php">SERIES</a></li>
+<li><a href="./../movies.php">PELICULAS</a></li>
+</ul>
+</div>
+<div id="Html1" style="position:absolute;left:0px;top:0px;width:33px;height:16px;z-index:9">
+<h6 id="viendo_ahora">VIENDO AHORA: </h6>
+
+</div>
+<div id="Html2" style="position:absolute;left:0px;top:0px;width:50px;height:30px;z-index:10">
+
+<img src="images/images/LOGO.png" class="logoweb" ></div>
+
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:872px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="wb_Shape1" style="position:absolute;left:608px;top:110px;width:22px;height:19px;z-index:13;">
+<a href="./../masterin.php"><img class="hover" src="images/img0004_hover.png" alt="" width="22" height="19" style="border-width:0;width:22px;height:19px;"><span class="default"><img src="images/img0004.png" id="Shape1" alt="" width="22" height="19" style="width:22px;height:19px;"></span></a></div>
+</div>
+
+</body>
+</html>
---
+++ demos/masterinfo.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv2.22.css" rel="stylesheet">
+<link href="masterinfo.css" rel="stylesheet">
+<link href="https://vjs.zencdn.net/7.19.2/video-js.css" rel="stylesheet" />
+{"statusCode":200,"status":"success","errorMessage":null}
+</head>
+<body>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:240px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="FlexGrid1">
+<div class="espacioleft1">
+</div>
+<div class="header">
+</div>
+<div class="espacio6">
+</div>
+<div class="header3">
+</div>
+<div class="section5">
+</div>
+<div class="espacio2">
+</div>
+<div class="espacio7">
+</div>
+<div class="espacio9">
+</div>
+<div class="espacio10">
+</div>
+<div class="section3">
+</div>
+<div class="espacio12">
+</div>
+<div class="section4">
+</div>
+<div class="espacio14">
+</div>
+<div class="footer">
+</div>
+<div class="time2">
+</div>
+<div class="time3">
+</div>
+<div class="time4">
+</div>
+<div class="time5">
+</div>
+<div class="section1">
+</div>
+<div class="espacio8">
+</div>
+<div class="espacio11">
+</div>
+<div class="espacio13">
+</div>
+<div class="espacio15">
+</div>
+<div class="section2">
+<table style="display:table;width:100%;height:16px;z-index:0;" id="Tableinfonow">
+<tr>
+<td class="cell0"><p style="font-family:Arial;font-size:13px;line-height:16px;"><span style="font-family:Agency FB;font-size:9.3px;line-height:12px;font-weight:bold;">&nbsp;</span><span style="font-family:Agency FB;font-size:9.3px;line-height:12px;">VIENDO AHORA</span><span style="font-family:Agency FB;font-size:11px;line-height:12.5px;color:#FFFF00;">:</span></p></td>
+<td class="cell1"><p>&nbsp;</p></td>
+</tr>
+</table>
+</div>
+<div class="viendo3">
+</div>
+</div>
+<!-- revisar este player -->
+<div id="Html1" style="position:absolute;left:0px;top:114px;width:100px;height:100px;z-index:3">
+revisar este player
+<video
+    id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    width="630"
+    height="360"
+    poster="https://images.viacbs.tech/uri/mgid:arc:imageassetref:shared.southpark.latam:60884349-742a-11ea-a59c-0a7527021758?quality=0.7&width=300&height=169&crop=true"
+    data-setup="{}"
+  >
+    <source src="https://vimn-ns1.ts.mtvnservices.com/v1/gsp.alias/mediabus/2020/06/03/07/22/12/8c1b0d487b6b411eb8d1c0f007a9751d/815610/0/seg_512x288_292871_36_3754015434.ts" type="application/octet-stream" />
+    <source src="https://vimn-ns1.ts.mtvnservices.com/v1/gsp.alias/mediabus/2020/06/03/07/22/12/8c1b0d487b6b411eb8d1c0f007a9751d/815610/0/seg_512x288_292871_36_3754015434.ts" type="video/MP2T" />
+    <source src="https://vimn-ns1.ts.mtvnservices.com/v1/gsp.alias/mediabus/2020/06/03/07/22/12/8c1b0d487b6b411eb8d1c0f007a9751d/815610/0/seg_512x288_292871_36_3754015434.ts" type="application/vnd.apple.mpegurl" />
+    <source src="https://vimn-ns1.ts.mtvnservices.com/v1/gsp.alias/mediabus/2020/06/03/07/22/12/8c1b0d487b6b411eb8d1c0f007a9751d/815610/0/seg_512x288_292871_36_3754015434.ts" type="application/x-mpegURL" />
+ 
+    
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a
+      web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank"
+        >supports HTML5 video</a
+      >
+    </p>
+  </video>
+
+  <script src="https://vjs.zencdn.net/7.19.2/video.min.js"></script></div>
+</body>
+</html>
