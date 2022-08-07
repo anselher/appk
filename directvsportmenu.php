fc03cb9cd99966e1d5561089ee7ca31e23f4d536
anselher <anselher@gmail.com>
2022-08-06 20:32:47 -0400

    Add files via upload

---
+++ directvsportmenu.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv2.22.css" rel="stylesheet">
+<link href="directvsportmenu.css" rel="stylesheet">
+<script>
+function disableIE() {
+    if (document.all) {
+        return false;
+    }
+}
+function disableNS(e) {
+    if (document.layers || (document.getElementById && !document.all)) {
+        if (e.which==2 || e.which==3) {
+            return false;
+        }
+    }
+}
+if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown = disableNS;
+} 
+else {
+    document.onmouseup = disableNS;
+    document.oncontextmenu = disableIE;
+}
+document.oncontextmenu=new Function("return false");
+</script>
+</head>
+<body>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:640px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="wb_LayoutGrid1">
+<div id="LayoutGrid1">
+<div class="row">
+<div class="col-1">
+<div id="wb_Breadcrumb1" style="display:inline-block;width:100%;z-index:0;vertical-align:top;">
+<ul id="Breadcrumb1">
+<li><a href="opcion-1" target="InlineFrameScreen" title="Opcion 1" onclick="window.open('comedycentralplay2','InlineFrameScreen');return false;">OPCION 1</a></li>
+<li><a href="opcion-2" title="Opcion 2" onclick="window.open('comedycentralplay1','InlineFrameScreen');return false;">OPCION 2</a></li>
+</ul>
+
+</div>
+</div>
+<div class="col-2">
+<div id="Html2" style="display:inline-block;width:37px;height:16px;z-index:1">
+<!-- Contador de visitas -->
+<img style="border: 0px solid; display: inline;" alt="contador de visitas" src="https://websmultimedia.com/contador-de-visitas.php?id=4463"></a>
+<!-- Fin Contador de visitas --></div>
+</div>
+<div class="col-3">
+<div id="wb_Breadcrumb4" style="display:inline-block;width:100%;z-index:2;vertical-align:top;">
+<ul id="Breadcrumb4">
+<li><a href="javascript:window.location.reload()" rel="nofollow noopener noreferrer sponsored ugc" target="InlineFrameScreen" title="Reload"><i class="fa fa-refresh">&nbsp;</i></a></li>
+</ul>
+
+</div>
+</div>
+<div class="col-4">
+</div>
+<div class="col-5">
+<div id="wb_Breadcrumb2" style="display:inline-block;width:100%;z-index:3;vertical-align:top;">
+<ul id="Breadcrumb2">
+<li><a href="opcion-1" target="InlineFrameScreen" title="Opcion 1" onclick="window.open('comedycentralplay2','InlineFrameScreen');return false;"><i class="fa fa-thumbs-o-down">&nbsp;</i> </a></li>
+<li><a href="javascript:window.location.reload()" rel="nofollow noopener noreferrer sponsored ugc" target="InlineFrameScreen" title="Reload"><i class="fa fa-thumbs-o-up">&nbsp;</i> </a></li>
+<li><a href=""><i class="fa fa-comment">&nbsp;</i> </a></li>
+</ul>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ directvsportplay1.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>page19</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv2.22.css" rel="stylesheet">
+<link href="directvsportplay1.css" rel="stylesheet">
+<script src="jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {$('#preloader').remove();}
+});
+$(window).on('load', function()
+{
+   $('#preloader').remove();
+});
+</script>
+<script src="https://cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/level-selector@latest/dist/level-selector.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/clappr-chromecast-plugin@latest/dist/clappr-chromecast-plugin.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/clappr-pip@latest/dist/clappr-pip.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/dash-shaka-playback@latest/dist/dash-shaka-playback.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/clappr-playback-rate-plugin@latest/dist/clappr-playback-rate-plugin.min.js"></script>
+
+</head>
+<body>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:630px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="player"></div>
+    <script>
+      var player = new Clappr.Player(
+        {
+          source: 'https://ed7.zorrohd2.nl/live/flowitc91zh.m3u8',
+          parentId: '#player',
+          plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback, Clappr.MediaControl],
+          events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},},
+          height: 353,
+          width: 628,
+          autoPlay: 'true',
+          shakaConfiguration: {
+          manifest: {retryParameters: {maxAttempts: Infinity}},
+          streaming: {retryParameters: {maxAttempts: Infinity}},
+          drm: {retryParameters: {maxAttempts: Infinity}},
+          preferredAudioLanguage: 'es-VE',
+          },
+          shakaOnBeforeLoad: function(shaka_player) {
+            // shaka_player.getNetworkingEngine().registerRequestFilter() ...
+          },
+          parentId: '#player'
+        });
+    </script>
+<div id="preloader"></div>
+</body>
+</html>
