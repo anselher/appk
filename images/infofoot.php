d0e6823543c8fe265b7c603a718637ef5bd85ab3
anselher <anselher@gmail.com>
2022-08-16 01:11:42 -0400

    Add files via upload

---
+++ images/infofoot.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<style>
+body
+{
+   background-color: transparent;
+   color: #000000;
+   font-family: "Barlow Condensed";
+   font-weight: 500;
+   font-size: 13px;
+   line-height: 1.1875;
+   margin: 0;
+   padding: 0;
+}
+a
+{
+   color: #F2F2F2;
+   text-decoration: none;
+}
+a:visited
+{
+   color: #FFFFFF;
+}
+a:active
+{
+   color: #FFFF00;
+}
+a:hover
+{
+   color: #00FF00;
+   text-decoration: none;
+}
+input:focus, textarea:focus, select:focus
+{
+   outline: none;
+}
+</style>
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv3.2.3.css?v=29" rel="stylesheet">
+<style>
+#wb_Image1
+{
+   vertical-align: top;
+}
+#Image1
+{
+   border: 0px solid #000000;
+   border-radius: 0px;
+   padding: 0;
+   margin: 0;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#wb_Image1
+{
+   position: absolute;
+   left: 0px;
+   top: 0px;
+   width: 871px;
+   height: 403px;
+   z-index: 1;
+}
+#wb_uid0
+{
+   position: absolute;
+   left: 441px;
+   top: 967px;
+   margin: 0;
+   border-width: 0;
+   z-index: 250;
+}
+</style>
+</head>
+<body>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" id="wb_uid0" width="16" height="16"></a>
+<div id="wb_Image1">
+<img src="images/Recurso%2037.png" id="Image1" alt="" width="871" height="403"></div>
+</body>
+</html>
---
+++ images/status-channel/status-vod.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Flag Colombia</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+body
+{
+   background-color: transparent;
+   color: #000000;
+   font-family: "Barlow Condensed";
+   font-weight: 500;
+   font-size: 13px;
+   line-height: 1.1875;
+   margin: 0;
+   padding: 0;
+}
+a
+{
+   color: #F2F2F2;
+   text-decoration: none;
+}
+a:visited
+{
+   color: #FFFFFF;
+}
+a:active
+{
+   color: #FFFF00;
+}
+a:hover
+{
+   color: #00FF00;
+   text-decoration: none;
+}
+input:focus, textarea:focus, select:focus
+{
+   outline: none;
+}
+</style>
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv3.2.3.css?v=45" rel="stylesheet">
+<style>
+#logochannelcnnFlexGrid1
+{
+   display: grid;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #CCCCCC;
+   box-sizing: border-box;
+   margin: 0;
+   padding: 3px 4px 1px 4px;
+   font-size: 0px;
+   grid-template-columns: 1fr;
+   grid-template-rows: 18px;
+   grid-template-areas:
+      "flex-flag";
+}
+#logochannelcnnFlexGrid1 .flex-flag
+{
+   display: flex;
+   grid-area: flex-flag;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   border-radius: 0px;
+   padding: 0;
+   -webkit-flex-direction: row;
+   flex-direction: row;
+   -webkit-flex-wrap: wrap;
+   flex-wrap: wrap;
+   -webkit-justify-content: space-around;
+   justify-content: space-around;
+   -webkit-align-items: center;
+   align-items: center;
+   -webkit-align-content: center;
+   align-content: center;
+}
+#wb_status-liveText1 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #000000;
+   border-radius: 0px;
+   padding: 0;
+   margin: 0;
+   text-align: center;
+}
+#wb_status-liveText1 div
+{
+   text-align: center;
+}
+#wb_status-liveText1
+{
+   display: block;
+   margin: 0;
+   box-sizing: border-box;
+   width: 100%;
+}
+#wb_uid1
+{
+   color: #7F7F7F;
+   font-family: "Barlow Condensed";
+   font-weight: 500;
+   font-size: 13px;
+   line-height: 18px;
+}
+#wb_uid0
+{
+   position: absolute;
+   left: 390px;
+   top: 0px;
+   margin: 0;
+   border-width: 0;
+   z-index: 250;
+}
+#Html1 img {
+   vertical-align:middle;
+   padding: 0 0px;
+   margin-left: 0px;
+   margin-top: 0px;
+   width: 18px;
+   height: 16px;
+   border-radius: 3px;
+   border-bottom: 0px solid #ccc;
+   border-top: 0px solid #4d4d4d;
+   border-left: 0px solid #808080;
+   border-right: 0px solid #808080;
+   object-fit: cover;
+}
+</style>
+</head>
+<body>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" id="wb_uid0" width="16" height="16"></a>
+<div id="logochannelcnnFlexGrid1">
+<div class="flex-flag">
+<div id="wb_status-liveText1">
+<span id="wb_uid1">VOD</span>
+</div>
+</div>
+</div>
+</body>
+</html>
