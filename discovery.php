57cd668cb9ade0a7bd9b957e3695cd3e0f005688
anselher <anselher@gmail.com>
2022-08-21 13:49:41 -0400

    Add files via upload

--- discovery.php
+++ discovery.php
@@ -54,7 +54,7 @@ input:focus, textarea:focus, select:focus
 </style>
 <link href="font-awesome.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
-<link href="365tv3.2.3.css?v=140" rel="stylesheet">
+<link href="365tv3.2.3.css?v=145" rel="stylesheet">
 <style>
 #InlineFrameBannerAds
 {
@@ -1217,6 +1217,7 @@ clock();
    <ul id="BreadcrumbMenu1">
       <li><a href="opcion-1" rel="nofollow noopener noreferrer sponsored ugc" target="InlineFrameScreen" title="Opcion 1" onclick="window.open('discoveryplay1','InlineFrameScreen');return false;">OPCION 1</a></li>
       <li><a href="opcion-2" rel="nofollow noopener noreferrer sponsored ugc" target="InlineFrameScreen" title="Opcion 2" onclick="window.open('discoveryplay2','InlineFrameScreen');return false;">OPCION 2</a></li>
+      <li><a href="opcion-3" rel="nofollow noopener noreferrer sponsored ugc" target="InlineFrameScreen" title="Opcion 3" onclick="window.open('discoverymaxplay1','InlineFrameScreen');return false;">DISCOVERY MAX</a></li>
       
    </ul>
 </div>
