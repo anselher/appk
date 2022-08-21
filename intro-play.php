6a7b6448166a5767876483a3d530226fd94c2e05
anselher <anselher@gmail.com>
2022-08-21 09:03:17 -0400

    Add files via upload

--- intro-play.php
+++ intro-play.php
@@ -49,33 +49,9 @@ input:focus, textarea:focus, select:focus
    z-index: 99999;
 }
 </style>
-<link href="plyr.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
-<link href="365tv3.2.3.css?v=134" rel="stylesheet">
+<link href="365tv3.2.3.css?v=143" rel="stylesheet">
 <style>
-#YouTube1
-{
-   position: absolute;
-   left: 0;
-   top: 0;
-   width: 100%;
-   height: 100%;
-   border-width: 0;
-}
-#wb_YouTube1
-{
-   background-color: #000000;
-   overflow: hidden !important;
-   position: fixed !important;
-   left: 0 !important;
-   top: 0 !important;
-   width: 100% !important;
-   height: 100% !important;
-}
-#wb_YouTube1
-{
-   --plyr-color-main: #4169E1;
-}
 #wb_uid0
 {
    position: absolute;
@@ -85,55 +61,20 @@ input:focus, textarea:focus, select:focus
    border-width: 0;
    z-index: 250;
 }
-#wb_YouTube1
+#page2Html1
 {
    position: absolute;
    left: 0px;
    top: 0px;
-   width: 727px;
-   height: 408px;
+   width: 729px;
+   height: 409px;
    z-index: 1;
 }
 </style>
 <script src="jquery-3.6.0.min.js"></script>
-<script src="plyr.min.js"></script>
-<script src="https://www.youtube.com/iframe_api"></script>
-<script>
-var playerYouTube1;
-function onYouTubeIframeAPIReady() {
-   playerYouTube1 = new YT.Player('YouTube1', {
-      events: {
-      }
-   });
-}
-</script>
 <script>
 $(document).ready(function()
 {
-   function resizeYouTube() 
-   {
-      var ratio = 16/9;
-      var widthViewPort = $(window).width();
-      var heightViewPort = $(window).height();
-      var width, height;
-      var $player = $('#YouTube1');
-      if (widthViewPort / ratio < heightViewPort)
-      {
-         width = Math.ceil(heightViewPort * ratio);
-         $player.width(width).height(heightViewPort).css({left: (widthViewPort - width) / 2, top: 0});
-      }
-      else
-      {
-         height = Math.ceil(widthViewPort / ratio);
-         $player.width(widthViewPort).height(height).css({left: 0, top: (heightViewPort - height) / 2});
-      }
-   }
-   $(window).on('resize', function() 
-   {
-      resizeYouTube();
-   })
-   resizeYouTube();
-   const plyrYouTube1 = new Plyr('#wb_YouTube1', {loadSprite: false, iconUrl: 'plyr.svg'});
    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {$('#preloader').remove();}
 });
 $(window).on('load', function()
@@ -141,14 +82,45 @@ $(window).on('load', function()
    $('#preloader').remove();
 });
 </script>
-<body onload="window.open('info-channel','InlineFrameInfoChannel');window.open('logo-channel-index','InlineFrameLogoChannel');window.open('./images/flags/flag-venezuela','InlineFrameFlags');window.open('./images/status-channel/status-live','InlineFrameStatusLive');document.getElementById('menumasterplayer').style.display='none';document.getElementById('menumasterplayer2').style.display='none';document.getElementById('wb_BreadcrumbMenuCentral').style.display='block';return false;">
+<script src="https://cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/level-selector@latest/dist/level-selector.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/clappr-chromecast-plugin@latest/dist/clappr-chromecast-plugin.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/clappr-pip@latest/dist/clappr-pip.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/dash-shaka-playback@latest/dist/dash-shaka-playback.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/clappr-playback-rate-plugin@latest/dist/clappr-playback-rate-plugin.min.js"></script>
+
+
 
 </head>
 <body>
 <a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" id="wb_uid0" width="16" height="16"></a>
-<div id="wb_YouTube1">
-<iframe id="YouTube1" src="https://www.youtube.com/embed/mYnA4kFMmIE?rel=0&amp;autoplay=1&amp;modestbranding=1&amp;enablejsapi=1&amp;autohide=0"></iframe>
-</div>
+<div id="page2Html1">
+<div id="player"></div>
+    <script>
+      var player = new Clappr.Player(
+        {
+          source: 'https://cdn-ue1-prod.tsv2.amagi.tv/linear/amg01024-olympusat-cinereallatam-ono/playlist.m3u8',
+          parentId: '#player',
+          plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback, Clappr.MediaControl],
+          events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},},
+          height: 410,
+          width: 729,
+          autoPlay: 'true',
+          shakaConfiguration: {
+          manifest: {retryParameters: {maxAttempts: Infinity}},
+          streaming: {retryParameters: {maxAttempts: Infinity}},
+          drm: {retryParameters: {maxAttempts: Infinity}},
+          preferredAudioLanguage: 'es-VE',
+          },
+          shakaOnBeforeLoad: function(shaka_player) {
+            // shaka_player.getNetworkingEngine().registerRequestFilter() ...
+          },
+          parentId: '#player'
+        });
+    </script>
+    
+    
+    </div>
 <div id="preloader"></div>
 </body>
 </html>
